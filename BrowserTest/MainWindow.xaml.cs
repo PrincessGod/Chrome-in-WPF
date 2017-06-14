322a2dd69cf0633f0c23fb754c2d78f9b0ee95bc
PrincessGod <jihongjiang@outlook.com>
2017-06-14 18:10:59 +0800

    Modify test example

---
+++ BrowserTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BrowserTest.Annotations;
+
+namespace BrowserTest
+{
+    /// <summary>
+    ///     MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        private string _adress = "www.google.com";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        public string Adress
+        {
+            get { return _adress; }
+
+            set
+            {
+                _adress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            uiBrowser.Closing();
+        }
+    }
+}
---
+++ NPUiBrowser/UiBrowser.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace NPUiBrowser
+{
+    /// <summary>
+    ///     用 CefSharp 57.0.0 的 WinForm 控件 封装成 WPF 用户控件
+    ///     可支持页面地址绑定
+    /// </summary>
+    public partial class UiBrowser
+    {
+        private static ChromiumWebBrowser _browser;
+
+        /// <summary>
+        ///     页面地址属性
+        /// </summary>
+        public static readonly DependencyProperty AdressProperty = DependencyProperty.Register(
+            "Adress", typeof(string), typeof(UiBrowser),
+            new PropertyMetadata("https://www.baidu.com/", PropertyChangedCallback));
+
+        /// <summary>
+        ///     Chrome 内核 WPF 浏览器
+        /// </summary>
+        public UiBrowser()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///     页面地址
+        /// </summary>
+        public string Adress
+        {
+            get { return (string) GetValue(AdressProperty); }
+            set { SetValue(AdressProperty, value); }
+        }
+
+        /// <summary>
+        ///     CefSharp 控件
+        /// </summary>
+        public ChromiumWebBrowser Browser => _browser;
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            _browser?.Load(dependencyPropertyChangedEventArgs.NewValue.ToString());
+        }
+
+        private void UiBrowser_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (_browser == null)
+                _browser = new ChromiumWebBrowser(Adress);
+            FormsHost.Child = _browser;
+        }
+
+        /// <summary>
+        ///     销毁控件  在关闭窗口前调用
+        /// </summary>
+        public void Closing()
+        {
+            Cef.Shutdown();
+            _browser = null;
+            FormsHost.Child = null;
+        }
+    }
+}
